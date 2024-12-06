42a03d8c476d54e74cbd8253392f699f27fe83fa
desanchezh <129230744+desanchezh@users.noreply.github.com>
2024-12-05 21:27:18 -0500

    Create driver script in main

---
+++ main.rb
@@ -0,0 +1,33 @@
+require_relative 'lib/tree'
+
+array = (Array.new(15) { rand(1..100) })
+tree = Tree.new(array)
+# Confirm that the tree is balanced by calling #balanced?
+puts "Is tree balanced?: #{tree.balanced?}"
+tree.pretty_print
+# Print out all elements in level, pre, post, and in order
+puts tree.level_order
+puts tree.preorder
+puts tree.postorder
+puts tree.inorder
+
+# Unbalance the tree by adding several numbers > 100
+tree.insert(1900)
+tree.insert(440)
+tree.insert(280)
+tree.insert(559)
+
+# Confirm that the tree is unbalanced by calling #balanced?
+puts "Is tree balanced?: #{tree.balanced?}"
+tree.pretty_print
+# Balance the tree by calling #rebalance
+tree.rebalance
+# Confirm that the tree is balanced by calling #balanced?
+puts "Is tree balanced?: #{tree.balanced?}"
+tree.pretty_print
+
+# Print out all elements in level, pre, post, and in order.
+puts tree.level_order
+puts tree.preorder
+puts tree.postorder
+puts tree.inorder
